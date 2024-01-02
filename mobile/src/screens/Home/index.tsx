19e47c37d9635fa93fc242f5d6d43bb8934156af
OCorvo <104469048+IsCorvo@users.noreply.github.com>
2024-01-02 18:47:33 +0000

    Commit Add Mobile mod 3

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,11 +1,12 @@
 import { View, Image, FlatList } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
-
-import { styles } from './styles';
-import { Heading } from '../../components/Heading';
 import { GameCard } from '../../components/GameCard';
+
+import { Heading } from '../../components/Heading';
+
 import { GAMES } from '../../utils/games';
+import { styles } from './styles';
 
 export function Home() {
     return (
